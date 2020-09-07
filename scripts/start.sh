e3c4963645e8212dd8fa67413b42a5ab3a1b1dcf
fernandogbi <fernandosilva.kadima@gmail.com>
2020-09-07 11:04:24 -0300

    add code to create credentials file in start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -f "./credentials.env" ]; then
+    echo "creating twitter credentials file..."
+    echo "ACCESS_TOKEN=" >> credentials.env
+    echo "ACCESS_TOKEN_SECRET=" >> credentials.env
+    echo "CONSUMER_KEY=" >> credentials.env
+    echo "CONSUMER_SECRET=" >> credentials.env
+fi
+
+
 echo "creating kafka and zookeeper services"
 
 CONFIG_FILE=$1 docker-compose up -d kafka
@@ -14,10 +23,6 @@ echo "creating flume service"
 
 if [ ! -d "./conf/flume/spool_to_kafka" ]; then
     mkdir "./conf/flume/spool_to_kafka"
-fi
-
-if [ ! -d "./conf/flume/spool_to_logger" ]; then
-    mkdir "./conf/flume/spool_to_logger"
 fi
 
 CONFIG_FILE=$1 docker-compose up -d flume
